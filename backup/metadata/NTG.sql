5a7878a185c1019b590bec98ad17c9ed2c04dfeb
pavelryzhikov <pavelryzhikov@gmail.com>
2015-04-17 11:44:01 +0300

    before release

--- backup/metadata/NTG.sql
+++ backup/metadata/NTG.sql
@@ -1,6 +1,28 @@
 /* ntg.airline  */
 
 
+
+   create or replace and compile
+   java source named "RandomUUID"
+   as
+   public class RandomUUID
+    {
+       public static String create()
+     {
+             return java.util.UUID.randomUUID().toString();
+     }
+   };
+/
+
+CREATE OR REPLACE FUNCTION RandomUUID
+   RETURN VARCHAR2
+    AS LANGUAGE JAVA
+    NAME 'RandomUUID.create() return java.lang.String';
+
+
+
+
+
 --------------------------------------------------------
 --  DDL for Table
 --------------------------------------------------------
